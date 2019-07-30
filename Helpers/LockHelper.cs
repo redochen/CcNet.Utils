98b639d43a095c8be8d6cbc3ac34cefd48327cac
Chen, Chang <charles.chen@yiako.com>
2019-07-30 14:22:40 +0800

    added lock helper and timer helper

---
+++ Helpers/LockHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+
+namespace CcNet.Utils.Helpers
+{
+    /// <summary>
+    /// 同步锁帮助类
+    /// </summary>
+    public class LockHelper
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public LockHelper()
+        {
+            _LockObj = new object();
+        }
+
+        /// <summary>
+        /// 锁定对象并执行方法
+        /// </summary>
+        /// <param name="lockTakenAction">获取锁后要执行的方法</param>
+        /// <param name="shouldEnterLock">检查是否要获取锁</param>
+        public void Lock(Action lockTakenAction, Func<bool> shouldEnterLock = null)
+            => Lock(takeLock: () =>
+            {
+                bool lockTaken = false;
+                Monitor.Enter(_LockObj, ref lockTaken);
+                return lockTaken;
+            }, lockTakenAction: lockTakenAction, shouldEnterLock: shouldEnterLock);
+
+        /// <summary>
+        /// 尝试锁定对象并执行方法
+        /// </summary>
+        /// <param name="lockTakenAction">获取锁后要执行的方法</param>
+        /// <param name="shouldEnterLock">检查是否要获取锁</param>
+        /// <param name="tryTimeout">尝试超时时间,为空表示永久等待</param>
+        public void TryLock(Action lockTakenAction, Func<bool> shouldEnterLock = null, TimeSpan? tryTimeout = null)
+            => Lock(takeLock: () =>
+            {
+                bool lockTaken = false;
+                if (tryTimeout.HasValue)
+                {
+                    Monitor.TryEnter(_LockObj, tryTimeout.Value, ref lockTaken);
+                }
+                else
+                {
+                    Monitor.TryEnter(_LockObj, ref lockTaken);
+                }
+                return lockTaken;
+            }, lockTakenAction: lockTakenAction, shouldEnterLock: shouldEnterLock);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="takeLock"></param>
+        /// <param name="lockTakenAction"></param>
+        /// <param name="shouldEnterLock"></param>
+        private void Lock(Func<bool> takeLock, Action lockTakenAction, Func<bool> shouldEnterLock = null)
+        {
+            if (!(shouldEnterLock?.Invoke() ?? true))
+            {
+                return;
+            }
+
+            if (!takeLock())
+            {
+                return;
+            }
+
+            try
+            {
+                if (!(shouldEnterLock?.Invoke() ?? true))
+                {
+                    return;
+                }
+
+                lockTakenAction();
+            }
+            finally
+            {
+                Monitor.Exit(_LockObj);
+            }
+        }
+
+        /// <summary>
+        /// 同步对象
+        /// </summary>
+        private readonly object _LockObj = null;
+    }
+}
---
+++ Helpers/TimerHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CcNet.Utils.Helpers
+{
+    /// <summary>
+    /// 计时器帮助类
+    /// </summary>
+    public class TimerHelper
+    {
+        private Timer _Timer = null;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="interval">间隔时间，单位：毫秒</param>
+        /// <param name="onTick">触发事件回调方法</param>
+        /// <param name="enabled">是否立即启用</param>
+        /// <param name="tag">自定义数据</param>
+        public TimerHelper(int interval, Action onTick, bool enabled, object tag = null)
+        {
+            _Timer = new Timer
+            {
+                Interval = interval,
+                Tag = tag,
+                Enabled = enabled
+            };
+
+            _Timer.Tick += (sender, e) => onTick();
+        }
+
+        /// <summary>
+        /// 获取自定义数据
+        /// </summary>
+        public object Tag => _Timer?.Tag;
+
+        /// <summary>
+        /// 获取计时器的当前状态
+        /// </summary>
+        public bool Enabled => _Timer?.Enabled ?? false;
+
+        /// <summary>
+        /// 启动计时器
+        /// </summary>
+        public void Start() => _Timer?.Start();
+
+        /// <summary>
+        /// 停止计时器
+        /// </summary>
+        public void Stop() => _Timer?.Stop();
+    }
+}
